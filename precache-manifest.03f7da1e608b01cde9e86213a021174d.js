a3f5ece6a63bb0b0d1bae395b73e71a189fe377d
Alaa Osman <alaa.osmana@gmail.com>
2020-09-28 01:05:48 +0200

    Updates

---
+++ precache-manifest.03f7da1e608b01cde9e86213a021174d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8881845b37f91d61a085d8873910a2b",
+    "url": "/timely/index.html"
+  },
+  {
+    "revision": "fd2bee3a3445a99f07b7",
+    "url": "/timely/static/css/2.63343480.chunk.css"
+  },
+  {
+    "revision": "6c753d21ecab4c5c135b",
+    "url": "/timely/static/css/main.9d65e660.chunk.css"
+  },
+  {
+    "revision": "fd2bee3a3445a99f07b7",
+    "url": "/timely/static/js/2.753ff6f4.chunk.js"
+  },
+  {
+    "revision": "3fd500bc1609e7226d38bfaedbd14763",
+    "url": "/timely/static/js/2.753ff6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c753d21ecab4c5c135b",
+    "url": "/timely/static/js/main.e24ab446.chunk.js"
+  },
+  {
+    "revision": "e944dd7654e2ac7d478e",
+    "url": "/timely/static/js/runtime-main.294fed8b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d28b28ae03eafda78acf1582e04c6e1e.js"
+  "/timely/precache-manifest.03f7da1e608b01cde9e86213a021174d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timely/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
